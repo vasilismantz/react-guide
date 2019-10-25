bf929eeb4f6d0d7803a50cb460bdc3b13e3b31e1
mantzarisvas <mantzarisvp@dias.com.gr>
2019-10-25 10:31:28 +0300

    Added componentWillUnmount Support

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -24,6 +24,10 @@ class Persons extends Component {
     console.log(snapshot);
   }
 
+  componentWillUnmount() {
+    console.log('[Persons.js] componentWillUmnount');
+  }
+
   render() {
     console.log('[Persons.js] rendering...');
     return this.props.persons.map((person, index) => {
