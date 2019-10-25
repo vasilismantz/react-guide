6abdbfc483675338274cde3f888ae92d68271102
mantzarisvas <mantzarisvp@dias.com.gr>
2019-10-25 10:31:06 +0300

    Added useEffect support

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.module.css';
 
-const cockpit = ( props ) => {
-    const assignedClasses = [];
-    let btnClass = '';
-    if (props.showPersons) {
-        btnClass = classes.Red;
-    }
+const Cockpit = ( props ) => {
 
-    if ( props.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
-    }
-    if ( props.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
-    }
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
 
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-            <button
-                className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
-        </div>
-    );
+    setTimeout(() => {
+      alert('Saved data to cloud');
+    }, 1000)
+  }, []);
+
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+      btnClass = classes.Red;
+  }
+
+  if ( props.persons.length <= 2 ) {
+    assignedClasses.push( classes.red ); // classes = ['red']
+  }
+  if ( props.persons.length <= 1 ) {
+    assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+  }
+
+  return (
+      <div className={classes.Cockpit}>
+          <h1>{props.title}</h1>
+          <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+          <button
+              className={btnClass}
+              onClick={props.clicked}>Toggle Persons</button>
+      </div>
+  );
 };
 
-export default cockpit;
+export default Cockpit;
